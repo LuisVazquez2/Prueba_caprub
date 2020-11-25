ae2639387fc1556f183a2ab8f03bd11799682962
Luis Vazquez <luisito986.22@gmail.com>
2020-11-24 18:42:11 -0600

    pointers
    djgbds

--- plntilla.cc
+++ plntilla.cc
@@ -4,7 +4,11 @@ using namespace std;
 #define v(type) vector<type>
 #define forn(var,n) for(int var=0;var<n;var++)
 #define forx(var,l,r) for(int var=l;var<r;var++)
-//typedef's
+#define rforn(var, n) for (int var = n; var >= 0;var--)
+#define rforx(var, l, r) for (int var = l; var >= r;var--)
+#define all(object) object.begin(), object.end()
+#define allr(object) object.rbegin(), object.rend()
+//typedef's 
 typedef unsigned int ui;
 typedef long long int lli;
 typedef unsigned long long int int64;
@@ -13,5 +17,9 @@ typedef v(lli) vlli;
 typedef v(int64) vi64;
 typedef v(ui) vui;
 int main(){
+    /*
+        1 3 2 // 1 2 3
+        r// 3 2 1
+    */
     return 0;
 }
