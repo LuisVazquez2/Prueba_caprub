c1b36dd83533afe73736c46c29f1d5bd508b826b
Luis Vazquez <luisito986.22@gmail.com>
2020-11-24 18:29:09 -0600

    chnage 2
    hola quebin

--- plntilla.cc
+++ plntilla.cc
@@ -1,8 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+//defines'
+#define v(type) vector<type>
+//typedef's
 typedef unsigned int ui;
 typedef long long int lli;
 typedef unsigned long long int int64;
+typedef v(int) vi;
+typedef v(lli) vlli;
 int main(){
     return 0;
 }
