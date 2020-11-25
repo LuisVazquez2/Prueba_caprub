2d8d180da32fb3327a86bd8da1ec591f3154887e
Kevin Uriel Manzano Rios <74994335+ElKPROOB@users.noreply.github.com>
2020-11-24 18:36:51 -0600

    xdd
    asdsadasdad

--- plntilla.cc
+++ plntilla.cc
@@ -2,12 +2,16 @@
 using namespace std;
 //defines'
 #define v(type) vector<type>
+#define forn(var,n) for(int var=0;var<n;var++)
+#define forx(var,l,r) for(int var=l;var<r;var++)
 //typedef's
 typedef unsigned int ui;
 typedef long long int lli;
 typedef unsigned long long int int64;
 typedef v(int) vi;
 typedef v(lli) vlli;
+typedef v(int64) vi64;
+typedef v(ui) vui;
 int main(){
     return 0;
 }
