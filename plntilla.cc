7f8bf1e76213ec83997613d54ad7e65eb74c90dd
Luis Vazquez <luisito986.22@gmail.com>
2020-11-24 18:52:51 -0600

    pairs
    jasjajs

--- plntilla.cc
+++ plntilla.cc
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 //defines'
+#define endl '\n';
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
 #define v(type) vector<type>
 #define forn(var,n) for(int var=0;var<n;var++)
 #define forx(var,l,r) for(int var=l;var<r;var++)
@@ -10,6 +14,7 @@ using namespace std;
 #define allr(object) object.rbegin(), object.rend()
 #define Csort(object) sort(all(object))
 #define Csortr(object) sort(allr(object))
+#define p(typeA ,typeB) pair<typeA,typeB> 
 //typedef's 
 typedef unsigned int ui;
 typedef long long int lli;
@@ -18,7 +23,8 @@ typedef v(int) vi;
 typedef v(lli) vlli;
 typedef v(int64) vi64;
 typedef v(ui) vui;
+typedef p(int, int) pii;
+typedef v(pii) vpii;
 int main(){
-
     return 0;
 }
